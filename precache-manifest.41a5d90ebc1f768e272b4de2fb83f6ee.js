d85822b8727d2c5ba72f93ee9eb6ed391364a244
bhaskarreddygurram <bhaskarreddygurram@gmail.com>
2020-06-23 11:20:09 +0200

    Updates

---
+++ precache-manifest.41a5d90ebc1f768e272b4de2fb83f6ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "184afca7200836b37650433bd5d122b2",
+    "url": "/React-Recipe-App/index.html"
+  },
+  {
+    "revision": "10b799a59da69cd13583",
+    "url": "/React-Recipe-App/static/css/main.754c8f9f.chunk.css"
+  },
+  {
+    "revision": "9faba460474174c92f69",
+    "url": "/React-Recipe-App/static/js/2.a0e2ec88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Recipe-App/static/js/2.a0e2ec88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b799a59da69cd13583",
+    "url": "/React-Recipe-App/static/js/main.1ede8323.chunk.js"
+  },
+  {
+    "revision": "f020c70b8e65be07257d",
+    "url": "/React-Recipe-App/static/js/runtime-main.47fef3f1.js"
+  }
+]);
