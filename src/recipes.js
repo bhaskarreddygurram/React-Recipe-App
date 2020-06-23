f8bfdee14a13f8b166ce5f4addefec67ae16b172
bhaskarreddygurram <bhaskarreddygurram@gmail.com>
2020-06-23 11:19:09 +0200

    first commit

--- src/recipes.js
+++ src/recipes.js
@@ -9,14 +9,14 @@ const Recipe = ({ title, calories, label, image, ingredients, healthLabels }) =>
     <div className='recipe'>
       <h1 className='recipe'>{title}</h1>
       <ol>
+        <h4>Ingredients:</h4>
         {ingredients.map((ingredient) => (
           <li>{ingredient.text}</li>
         ))}
       </ol>
-      <p>{calories}</p>
+      <p>Calories:  {calories}</p>
       <img className='image' src={image} alt='' />
       <p>{label}</p>
-      <p>{healthLabels[(1, 2, 3)]}</p>
     </div>
   );
 };
